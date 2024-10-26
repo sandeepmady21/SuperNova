41e47c3b6cb2fc4660360fafebeac4bf7a371595
michaeltorkaman <michael.torkaman.t@gmail.com>
2024-10-26 19:26:07 +0200

    Removed Swif files and basic restructuring of files

---
+++ Algorithm/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+type Element struct {
+	parentID int
+	elementID int
+	content Content
+	priority int
+	/* uniqueId for each element type
+	* 0 = Article type
+	* 1 = Item type
+	* 2 = Tasks
+	*/
+	elementType int
+	interval int
+	grade int
+	EF  float64
+	//prevRepDate time.Time //TODO need to update prevRepDate and nextRepDate for every update
+	nextRepDate time.Time
+	templateId int
+}
+
+type Content struct {
+ 	imageIDs 	[]int
+ 	textIDs		[]int
+ 	referenceID	int
+ 	videoIDs	[]int
+}
+
+
+type Reference struct {
+	title        string
+	author       string
+	date         time.Time
+	source       string
+	link         string
+	parentTitle  string
+	conceptGroup string
+}
+
+
+
+func main() {
+	date := time.Now()
+	myElement := Element{
+		interval: 4,
+		grade:    3,
+		EF:       1.7,
+		nextRepDate: date,}
+	fmt.Printf("%+v\n", myElement)
+	fmt.Println()
+	fmt.Println("updating element")
+	updateLearningData(&myElement)
+	fmt.Printf("%+v\n", myElement)
+}
+
+func showContent(element *Element){
+	for 
+	
+
+func updateLearningData(element *Element){
+	element.EF = calculateNewEF(element.EF, element.grade)
+	element.interval = calculateNewInterval(element.EF, element.interval)
+	element.nextRepDate = calculateNextRepDate(element.nextRepDate, element.interval)
+}
+	
+
+func calculateNewInterval(newEF float64, oldInterval int) int{
+	newInterval := int(math.Round(newEF * float64(oldInterval)))
+	return newInterval
+}
+
+	// takes in prev rep date and interval
+	// returns next rep date
+func calculateNextRepDate(prevRepDate time.Time, interval int) time.Time {
+	nextRepDate := prevRepDate.Add(time.Duration(interval) * 24 * time.Hour)
+	return nextRepDate
+}
+
+	// takes in oldEF and grade
+	// returns newEF
+func calculateNewEF(oldEF float64, grade int) float64 {
+	newEF := oldEF - 0.8 + 0.28 * float64(grade) - 0.02 * float64(math.Pow(float64(grade), 2))
+	return newEF
+}
+ // michael was here 
